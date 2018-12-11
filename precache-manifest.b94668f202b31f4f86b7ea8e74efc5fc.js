b7d5925cb301adcdfbe879fc0c4984fd711c7785
krizebcev <krizebcev@foi.hr>
2018-12-12 00:32:11 +0100

    Updates

---
+++ precache-manifest.b94668f202b31f4f86b7ea8e74efc5fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "ab2a4618acf924579364",
+    "url": "/contact-manager/static/js/main.ab2a4618.chunk.js"
+  },
+  {
+    "revision": "fb6903c35e0587305961",
+    "url": "/contact-manager/static/js/1.fb6903c3.chunk.js"
+  },
+  {
+    "revision": "ab2a4618acf924579364",
+    "url": "/contact-manager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "fb6903c35e0587305961",
+    "url": "/contact-manager/static/css/1.515c0bfb.chunk.css"
+  },
+  {
+    "revision": "4a1fa51b6af8b3106aef4f0a62ee8c44",
+    "url": "/contact-manager/index.html"
+  }
+];
